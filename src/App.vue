9ca1a3443857c34dd1de6f5293d93a85b2e21391
kevinokello <okellokevin77@gmail.com>
2023-06-21 13:25:51 +0300

    styling

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,14 @@
 
 
 <script>
-import Master from "./views/Master.vue"
+import Master from "./views/Master.vue";
+import Contact from "./views/pages/Contact.vue";
+import Projects from "./views/pages/Projects.vue";
+import Resume from "./views/pages/Resume.vue";
+
 
 export default {
-  components: { Master },
+  components: { Master, Contact, Projects, Resume },
 };
 </script>
 
