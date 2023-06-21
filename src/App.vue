d5806ec3b924b6a424d219f44dfea83b715450e5
kevinokello <okellokevin77@gmail.com>
2023-06-21 13:09:27 +0300

    master

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<master></master>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
+<script>
+import Master from "./views/Master.vue"
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+export default {
+  components: { Master },
+};
+</script>
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+
+<style>
+
+
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Master from '../views/Master.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Master',
+    component: Master
   },
   {
     path: '/about',
